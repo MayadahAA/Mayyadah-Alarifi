5f8e85bb4044125b1a53ca5513fba912dd341739
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2023-01-26 15:01:00 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function toggleDarkMode() {
 }
 
 toggleDarkModeButton.addEventListener("click", toggleDarkMode);
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
